2771bead991cae5dbc4e4865bc8ab659c5595eb1
Eduardo Yaguar <eduardoyaguarf@gmail.com>
2024-03-19 17:00:11 -0500

    Added TextBox in MyPlotFrame
    Added Textbox in ScrollFrame

--- App.py
+++ App.py
@@ -4,12 +4,14 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 class MyPlotFrame(customtkinter.CTkScrollableFrame):
-    def __init__(self,master,title):
+    def __init__(self,master,title, desc):
         super().__init__(master)
         self.grid_columnconfigure(0,weight=1)
-        self.grid_rowconfigure((0,1), weight=1)
+        self.grid_rowconfigure((0,1,2), weight=1)
         self.title = title
+        self.desc = desc
         self.figure, self.ax = plt.subplots()
+        
         
 
         canvas = FigureCanvasTkAgg(self.figure, self)
@@ -21,6 +23,10 @@ class MyPlotFrame(customtkinter.CTkScrollableFrame):
 
         canvas.get_tk_widget().grid(row=1, column=0, padx=10, pady=(10,0), sticky="ew")
 
+        textBox = customtkinter.CTkTextbox(self, fg_color="gray30", corner_radius=6)
+        textBox.insert(index="0.0",text=self.desc)
+        textBox.grid(row=3, column=0, padx=10, pady=(10,0), sticky="nsew",)
+
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
@@ -30,21 +36,21 @@ class App(customtkinter.CTk):
         self.grid_rowconfigure((0,1),weight=1)
         self.grid_columnconfigure((0,1),weight=1)
 
-        self.MyPlotFrame1 = MyPlotFrame(self, title="Ventas por territorio 2014")
+        self.MyPlotFrame1 = MyPlotFrame(self, title="Ventas por territorio 2014", desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque varius, dui sit amet iaculis convallis, nulla lectus varius turpis, vitae fringilla dui mi iaculis mi. Donec et mattis urna, vel vestibulum diam. Suspendisse nec pellentesque felis. Nunc in volutpat libero, nec condimentum velit. Nullam quis quam eget felis convallis porta. Mauris porttitor, nunc ut aliquet posuere, leo lectus aliquam est, eu luctus magna dolor non nibh. Aliquam erat volutpat. Integer ultricies suscipit lacus. Sed eu massa non enim lobortis elementum. Cras tempus finibus nunc, sit amet pretium nisl mattis quis. Cras id tristique leo. Phasellus massa purus, rhoncus et diam.")
         self.MyPlotFrame1.grid(row=0,column=0,padx=10,pady=(10,0), sticky="nsew")
 
 
         
 
-        self.MyPlotFrame2 = MyPlotFrame(self, title="Categorias más vendidas en el territorio X") #Siendo x el territorio con mayor ganacias en 2014
+        self.MyPlotFrame2 = MyPlotFrame(self, title="Categorias más vendidas en el territorio X", desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque varius, dui sit amet iaculis convallis, nulla lectus varius turpis, vitae fringilla dui mi iaculis mi. Donec et mattis urna, vel vestibulum diam. Suspendisse nec pellentesque felis. Nunc in volutpat libero, nec condimentum velit. Nullam quis quam eget felis convallis porta. Mauris porttitor, nunc ut aliquet posuere, leo lectus aliquam est, eu luctus magna dolor non nibh. Aliquam erat volutpat. Integer ultricies suscipit lacus. Sed eu massa non enim lobortis elementum. Cras tempus finibus nunc, sit amet pretium nisl mattis quis. Cras id tristique leo. Phasellus massa purus, rhoncus et diam.") #Siendo x el territorio con mayor ganacias en 2014
         self.MyPlotFrame2.grid(row=0,column=1,padx=10,pady=(10,0), sticky="nsew")
 
         
-        self.MyPlotFrame3 = MyPlotFrame(self, title="Categorias más vendidas en el territorio Y") #Siendo y el territorio con menos ventas en 2014
+        self.MyPlotFrame3 = MyPlotFrame(self, title="Categorias más vendidas en el territorio Y", desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque varius, dui sit amet iaculis convallis, nulla lectus varius turpis, vitae fringilla dui mi iaculis mi. Donec et mattis urna, vel vestibulum diam. Suspendisse nec pellentesque felis. Nunc in volutpat libero, nec condimentum velit. Nullam quis quam eget felis convallis porta. Mauris porttitor, nunc ut aliquet posuere, leo lectus aliquam est, eu luctus magna dolor non nibh. Aliquam erat volutpat. Integer ultricies suscipit lacus. Sed eu massa non enim lobortis elementum. Cras tempus finibus nunc, sit amet pretium nisl mattis quis. Cras id tristique leo. Phasellus massa purus, rhoncus et diam.") #Siendo y el territorio con menos ventas en 2014
         self.MyPlotFrame3.grid(row=1,column=0,padx=10,pady=(10,0), sticky="nsew")
 
         
-        self.MyPlotFrame4 = MyPlotFrame(self, title="Categorias con menos ventas en 2014")
+        self.MyPlotFrame4 = MyPlotFrame(self, title="Categorias con menos ventas en 2014", desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque varius, dui sit amet iaculis convallis, nulla lectus varius turpis, vitae fringilla dui mi iaculis mi. Donec et mattis urna, vel vestibulum diam. Suspendisse nec pellentesque felis. Nunc in volutpat libero, nec condimentum velit. Nullam quis quam eget felis convallis porta. Mauris porttitor, nunc ut aliquet posuere, leo lectus aliquam est, eu luctus magna dolor non nibh. Aliquam erat volutpat. Integer ultricies suscipit lacus. Sed eu massa non enim lobortis elementum. Cras tempus finibus nunc, sit amet pretium nisl mattis quis. Cras id tristique leo. Phasellus massa purus, rhoncus et diam.")
         self.MyPlotFrame4.grid(row=1,column=1,padx=10,pady=(10,0), sticky="nsew")
 app = App()
 app.mainloop()
