4a65d30c505334939095808887e7ec8948645c66
Eduardo Yaguar <eduardoyaguarf@gmail.com>
2024-03-30 15:07:57 -0500

    Update App Data and Plots
    Added Data to plots for drawing

--- App.py
+++ App.py
@@ -36,10 +36,16 @@ class MyPlotFrame(customtkinter.CTkScrollableFrame):
         yData = [item[0] for item in yData]
 
         self.ax.bar(xData, yData)
+        
 
         self.ax.set_xticks(range(len(xData)))
-        self.ax.set_xticklabels(xData, rotation=20, ha='right')
+        self.ax.set_xticklabels(xData, rotation=15, ha='center', fontsize="small")
+        self.ax.grid()
+        self.ax.yaxis.grid(color='gray', linestyle='--', linewidth=0.5, alpha=0.8)  
+        self.ax.xaxis.grid(color="none")
 
+        print(  xData, "\n")
+        print(  yData, "\n")
         canvas = FigureCanvasTkAgg(self.figure, self)
         canvas.draw()   
         #Puedo hacer la creación del objeto con el matplot en blanco y a traves de metodos hago los cambios de valores
@@ -52,7 +58,7 @@ class MyPlotFrame(customtkinter.CTkScrollableFrame):
         textBox = customtkinter.CTkTextbox(self, fg_color="gray30", corner_radius=6, cursor="arrow")
         textBox.insert(index="0.0",text=self.desc)
         textBox.configure(state="disable")
-        textBox.grid(row=3, column=0, padx=10, pady=(10,0), sticky="nsew",)
+        textBox.grid(row=3, column=0, padx=10, pady=(15,0), sticky="nsew",)
 
 class App(customtkinter.CTk):
     def __init__(self):
@@ -79,15 +85,44 @@ class App(customtkinter.CTk):
         self.MyPlotFrame1 = MyPlotFrame(self, title="Ventas por territorio 2014", xData=result1, yData=result2 ,desc="Lorem ipsvolutpat. Integer s tempus finibu massa purus, rhoncus et diam.")
         self.MyPlotFrame1.grid(row=0,column=0,padx=10,pady=(10,0), sticky="nsew")
 
+        result1 = db.executeQuery(text('SELECT TOP 10 pro.NombreProducto FROM DimProductos as pro INNER JOIN DimDetalles_Orden as deo ON pro.IDProducto = deo.IDProducto INNER JOIN Hechos_Ordenes as ord ON deo.IDOrden = ord.IDOrden GROUP BY pro.NombreProducto ORDER BY SUM(ord.TotalOrden) DESC;'))
+        x = []
+        for rows in result1:
+            x.extend(rows)
+        y = []
+        result2 = db.executeQuery(text('SELECT TOP 10 SUM(ord.TotalOrden) as totalVentas FROM DimProductos as pro INNER JOIN DimDetalles_Orden as deo ON pro.IDProducto = deo.IDProducto INNER JOIN Hechos_Ordenes as ord ON deo.IDOrden = ord.IDOrden GROUP BY pro.NombreProducto ORDER BY totalVentas DESC;'))
+        for rows in result2:
+            value = int(rows[0])
+            y.append(value)
 
-        self.MyPlotFrame2 = MyPlotFrame(self, title="Categorias más vendidas en el territorio X", xData=result1, yData=result2 ,desc="Lorem t amet iaculis con")
+        self.MyPlotFrame2 = MyPlotFrame(self, title="Top 10 Productos Más Vendidos", xData=result1, yData=result2 ,desc="Lorem t amet iaculis con")
         self.MyPlotFrame2.grid(row=0,column=1,padx=10,pady=(10,0), sticky="nsew")
         
-        self.MyPlotFrame3 = MyPlotFrame(self, title="Categorias más vendidas en el territorio Y", xData=result1, yData=result2 , desc="Lorem iit. Pnullibero elemeetisellus massa purus, rhoncus et diam.") #Siendo y el territorio con menos ventas en 2014
+        result1 = db.executeQuery(text('SELECT tie.TRIMESTRE FROM DimTiempo as tie INNER JOIN Hechos_Ordenes as ord ON tie.IDOrden = ord.IDOrden GROUP BY tie.TRIMESTRE ORDER BY SUM(ord.TotalOrden) DESC;'))
+        x = []
+        for rows in result1:
+            x.extend(rows)
+        
+        y = []
+        result2 = db.executeQuery(text('SELECT SUM(ord.TotalOrden) as Total_Ventas FROM DimTiempo as tie INNER JOIN Hechos_Ordenes as ord ON tie.IDOrden = ord.IDOrden GROUP BY tie.TRIMESTRE ORDER BY Total_Ventas DESC;'))
+        for rows in result2:
+            value = int(rows[0])
+            y.append(value)
+
+        self.MyPlotFrame3 = MyPlotFrame(self, title="Ventas Por Trimestre", xData=result1, yData=result2 , desc="Lorem iit. Pnullibero elemeetisellus massa purus, rhoncus et diam.") #Siendo y el territorio con menos ventas en 2014
         self.MyPlotFrame3.grid(row=1,column=0,padx=10,pady=(10,0), sticky="nsew")
 
-        
-        self.MyPlotFrame4 = MyPlotFrame(self, title="Categorias con menos ventas en 2014", xData=result1, yData=result2 , desc="Lor libero, nec coeo. Phasellus massa purus, rhoncus et diam.")
+        result1 = db.executeQuery(text("SELECT TOP 10 CONCAT(cli.Primer_Nombre, ' ', cli.Apellido) AS Nombre_Completo FROM DimClientes AS cli INNER JOIN Hechos_Ordenes as ord ON cli.IDCliente = ord.IDCliente GROUP BY cli.Primer_Nombre, cli.Apellido ORDER BY SUM(ord.TotalOrden) DESC;"))
+        x = []
+        for rows in result1:
+            x.extend(rows)
+        y = []
+        result2 = db.executeQuery(text('SELECT TOP 10 SUM(ord.TotalOrden) as total_Ventas FROM DimClientes AS cli INNER JOIN Hechos_Ordenes as ord ON cli.IDCliente = ord.IDCliente GROUP BY cli.Primer_Nombre, cli.Apellido ORDER BY SUM(ord.TotalOrden) DESC;'))
+        for rows in result2:
+            value = int(rows[0])
+            y.append(value)
+
+        self.MyPlotFrame4 = MyPlotFrame(self, title="Ventas por Cliente", xData=result1, yData=result2 , desc="Lor libero, nec coeo. Phasellus massa purus, rhoncus et diam.")
         self.MyPlotFrame4.grid(row=1,column=1,padx=10,pady=(10,0), sticky="nsew")
 
         db.disconnect()
